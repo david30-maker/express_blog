8c3d3b513e5b59dacd936f2aa827afea5f62f69c
david30-maker <davidokpah396@gmail.com>
2024-07-21 18:20:53 +0100

    add models and router for blog application

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const articleRouter = require('./routes/articles');
+const app = express();
+
+// MongoDB connection string
+const mongoURL = 'mongodb://localhost:27017/blog';
+
+mongoose.connect(mongoURL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  const articles = [
+    {
+      title: 'Test Article',
+      createdAt: new Date(),
+      description: 'Test description',
+    },
+    {
+      title: 'Test Article 2',
+      createdAt: new Date(),
+      description: 'Test description 2',
+    },
+  ];
+  res.render('articles/index', { articles: articles });
+});
+
+app.use('/articles', articleRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
