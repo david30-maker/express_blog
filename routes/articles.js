105352453aa8507465b5833a486d48f5ae1edd32
david30-maker <davidokpah396@gmail.com>
2024-07-25 21:02:34 +0100

    update router.js

--- routes/articles.js
+++ routes/articles.js
@@ -2,12 +2,10 @@ const express = require('express');
 const Article = require('../models/article');
 const router = express.Router();
 
-// Route for rendering the new article form
 router.get('/new', (req, res) => {
     res.render('articles/new', { article: {} });
 });
 
-// Route for rendering the edit article form
 router.get('/:id/edit', async (req, res) => {
     const article = await Article.findById(req.params.id);
     if (article) {
@@ -17,7 +15,6 @@ router.get('/:id/edit', async (req, res) => {
     }
 });
 
-// Route for displaying an article based on its slug
 router.get('/:slug', async (req, res) => {
     try {
         const article = await Article.findOne({ slug: req.params.slug });
@@ -31,25 +28,21 @@ router.get('/:slug', async (req, res) => {
     }
 });
 
-// Route for creating a new article
 router.post('/', async (req, res, next) => {
     req.article = new Article();
     next();
 }, saveArticleAndRedirect('new'));
 
-// Route for updating an existing article
 router.put('/:id', async (req, res, next) => {
     req.article = await Article.findById(req.params.id);
     next();
 }, saveArticleAndRedirect('edit'));
 
-// Route for deleting an article
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id);
     res.redirect('/');
 });
 
-// Function to handle saving an article and redirecting
 function saveArticleAndRedirect(path) {
     return async (req, res) => {
         let article = req.article;
